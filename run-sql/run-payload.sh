62b1a248df4c0ad19c83c776585395979a31091b
Rudy Widjaja <rwidjaja@hotmail.com>
2024-07-20 22:00:32 -0700

    Delete run-sql/run-payload.sh

--- run-sql/run-payload.sh
+++
@@ -1,7 +0,0 @@
-export jwt=`curl --insecure -s -X GET -u admin:password "https://ubuntu-atscale.atscaledomain.com:10500/default/auth"`
-
-curl --insecure -X POST \
--H "Authorization: Bearer $jwt" \
--H "Content-Type: application/xml" \
---data @payload.xml \
-https://ubuntu-atscale.atscaledomain.com:10502/xmla/default
